ccfcbf93d6e88f3ce5a87a798a05bc4d54a63e8a
misaellemuz <lemuzmisael@outlook.com>
2024-08-10 01:52:16 -0700

    Add files via upload

---
+++ precache-manifest.43e21ff6e2fc533c316944686ea83d46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0235b0f3db7e20ea8e7dd2cdeded9dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13a236cbbff701ff033f",
+    "url": "/static/css/main.45e54649.chunk.css"
+  },
+  {
+    "revision": "3b145d8447431d6c1eae",
+    "url": "/static/js/2.f8e7eee4.chunk.js"
+  },
+  {
+    "revision": "1ac23edd848720c87dd7eac90d67e14d",
+    "url": "/static/js/2.f8e7eee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a236cbbff701ff033f",
+    "url": "/static/js/main.647b13b7.chunk.js"
+  },
+  {
+    "revision": "cc1b1223b74e90d4b234",
+    "url": "/static/js/runtime-main.e382795e.js"
+  },
+  {
+    "revision": "f5299e261a7e143b4408e81baef0c72e",
+    "url": "/static/media/add.f5299e26.svg"
+  },
+  {
+    "revision": "b135e21dea8e193c60e96e151c70c138",
+    "url": "/static/media/arrow-left.b135e21d.svg"
+  }
+]);
